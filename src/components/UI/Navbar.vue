a106f3cfcd76cab489c653b3435b03479a2be35a
Georg <g3orgros@gmail.com>
2023-03-10 00:26:51 +0300

    basic routing/static navigation added

---
+++ src/components/UI/Navbar.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="navbar">
+        <div @click="$router.push('/')"
+        >Vue 3 test app</div>
+        <div class="navbar__btns">
+            <my-button
+             @click="$router.push('/posts')"
+             >Посты</my-button>
+            <my-button 
+            @click="$router.push('/about')"
+            >О сайте</my-button>
+
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    .navbar {
+        height: 50px;
+        background-color: lightgray;
+        box-shadow: 2px 2px 4px gray;
+        display: flex;
+        align-items: center;
+        
+    }
+    .navbar__btns{
+        display: flex;
+        flex-direction: row;
+        text-decoration: none;
+        gap: 10px;
+        margin-left: auto;
+    }
+    
+</style>
---
+++ src/pages/Main.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+        <h1>MainPage</h1>
+        <div>
+            <router-link to="posts">Посты</router-link>
+        </div>
+        <div>
+            <router-link to="about">О сайте</router-link>
+        </div>
+        
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style>
+    
+</style>
